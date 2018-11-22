a0d736d034d8d2cef6232136a228663a3619b2eb
July Slavhev <jul.slavchev.tues19@gmail.com>
2018-11-22 09:14:37 +0200

    ListView layout is ready.

--- app/src/main/java/com/example/user/jobche/HomeActivity.kt
+++ app/src/main/java/com/example/user/jobche/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class HomeActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
