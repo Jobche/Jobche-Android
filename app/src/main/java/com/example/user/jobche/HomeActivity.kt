b8363220c430aa18795c62fafedf59f2b041e953
July Slavhev <jul.slavchev.tues19@gmail.com>
2018-11-24 19:07:04 +0200

    Home layout Floating Action Button added.

--- app/src/main/java/com/example/user/jobche/HomeActivity.kt
+++ app/src/main/java/com/example/user/jobche/HomeActivity.kt
@@ -2,6 +2,8 @@ package com.example.user.jobche
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class HomeActivity : AppCompatActivity() {
 
@@ -9,5 +11,6 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
     }
 }
