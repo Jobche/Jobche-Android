cac194407b93fcf22fca336c5cf1b862fa53c718
July Slavhev <jul.slavchev.tues19@gmail.com>
2018-11-27 11:19:15 +0200

    Sign up activity for birth date and email created.

---
+++ app/src/main/java/com/example/user/jobche/SignupBirthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.user.jobche
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupBirthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_birth)
+    }
+}
