15f615b90902584febad270b281cf200da31e1e3
July Slavhev <jul.slavchev.tues19@gmail.com>
2018-11-27 11:36:27 +0200

    Sign up activity for birth date and email onClick event created.

--- app/src/main/java/com/example/user/jobche/SignupBirthActivity.kt
+++ app/src/main/java/com/example/user/jobche/SignupBirthActivity.kt
@@ -1,12 +1,20 @@
 package com.example.user.jobche
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignupBirthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_birth)
+
+        val mNext = findViewById<Button>(R.id.next_birth_btn)
+        mNext.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
