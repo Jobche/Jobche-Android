ffa9dbdbbf752f598752b8403d42830909df4645
July Slavhev <jul.slavchev.tues19@gmail.com>
2018-11-27 10:55:21 +0200

    Sign up with name information activity button, onclick event created.

--- app/src/main/java/com/example/user/jobche/SignupNameActivity.kt
+++ app/src/main/java/com/example/user/jobche/SignupNameActivity.kt
@@ -1,12 +1,20 @@
 package com.example.user.jobche
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignupNameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_name)
+
+        val mNext = findViewById<Button>(R.id.next_name_btn)
+        mNext.setOnClickListener {
+            val intent = Intent(this, SignupPasswordActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
