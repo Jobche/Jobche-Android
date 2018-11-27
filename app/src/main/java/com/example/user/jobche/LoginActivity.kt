2a5129f8dee8e39066be0429ef9d5d7754f584e0
July Slavhev <jul.slavchev.tues19@gmail.com>
2018-11-27 09:43:11 +0200

    OnClick events on Log in and Sign up in Login layout

--- app/src/main/java/com/example/user/jobche/LoginActivity.kt
+++ app/src/main/java/com/example/user/jobche/LoginActivity.kt
@@ -1,12 +1,26 @@
 package com.example.user.jobche
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val mLogin = findViewById<Button>(R.id.login_btn)
+        mLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        val mSignup = findViewById<Button>(R.id.login_btn)
+        mLogin.setOnClickListener {
+            val intent = Intent(this, SignupNameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
