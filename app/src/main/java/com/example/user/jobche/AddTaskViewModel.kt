9801627ea9eba3ac01a176af790190cbe3b45f2d
July Slavhev <jul.slavchev.tues19@gmail.com>
2019-01-11 16:53:57 +0200

    TimePickerDialog and DatePickerDialog added in AddTask, they are called when the proper edittext is clicked.

--- app/src/main/java/com/example/user/jobche/AddTaskViewModel.kt
+++ app/src/main/java/com/example/user/jobche/AddTaskViewModel.kt
@@ -1,15 +1,41 @@
 package com.example.user.jobche
 
+import android.app.DatePickerDialog
 import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
 import androidx.lifecycle.LiveData
 
 class AddTaskViewModel: BaseObservable() {
+
+    private var date:String = ""
+
+    private var time:String = ""
 
     private val _dateEventLiveData = SingleLiveData<Any>()
 
     private val _timeEventLiveData = SingleLiveData<Any>()
 
     private val _addTaskEventLiveData = SingleLiveData<Any>()
+
+    @Bindable
+    fun getDate(): String {
+        return this.date
+    }
+
+    fun setDate(date:String) {
+        this.date = date
+        notifyPropertyChanged(BR.date)
+    }
+
+    @Bindable
+    fun getTime(): String {
+        return this.time
+    }
+
+    fun setTime(time:String) {
+        this.time = time
+        notifyPropertyChanged(BR.time)
+    }
 
     val dateEventLiveData : LiveData<Any>
         get() = _dateEventLiveData
@@ -20,6 +46,13 @@ class AddTaskViewModel: BaseObservable() {
     val addTaskEventLiveData: LiveData<Any>
         get() = _addTaskEventLiveData
 
+    fun onClickDate() {
+        _dateEventLiveData.call()
+    }
+
+    fun onClickTime() {
+        _timeEventLiveData.call()
+    }
 
     fun onClickAddTask() {
         _addTaskEventLiveData.call()
--- app/src/main/java/com/example/user/jobche/UI/AddTaskActivity.kt
+++ app/src/main/java/com/example/user/jobche/UI/AddTaskActivity.kt
@@ -1,11 +1,23 @@
 package com.example.user.jobche.UI
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import com.example.user.jobche.AddTaskViewModel
+import com.example.user.jobche.HomeViewModel
 import com.example.user.jobche.R
+import com.example.user.jobche.databinding.ActivityAddTaskBinding
+import com.example.user.jobche.databinding.ActivityHomeBinding
 import kotlinx.android.synthetic.main.toolbar.*
+import java.time.Month
+import java.util.*
+import javax.xml.datatype.DatatypeConstants.MONTHS
 
 class AddTaskActivity : AppCompatActivity() {
 
@@ -14,10 +26,40 @@ class AddTaskActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_task)
         toolbar_title.text="Add Task"
 
-        val mAddTask = findViewById<Button>(R.id.add_task_btn)
-        mAddTask.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-        }
+        val binding: ActivityAddTaskBinding = DataBindingUtil.setContentView(this, R.layout.activity_add_task)
+        val addTaskViewModel = AddTaskViewModel()
+        binding.viewModel = addTaskViewModel
+
+        addTaskViewModel.dateEventLiveData.observe(this, Observer {
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+
+                // Display Selected date in textbox
+                addTaskViewModel.setDate(String.format("%02d", dayOfMonth) + "-" + String.format("%02d", (monthOfYear + 1)) + "-" + year)
+            }, year, month, day)
+            dpd.show()
+        })
+
+        addTaskViewModel.timeEventLiveData.observe(this, Observer {
+            val c = Calendar.getInstance()
+            val hour = c.get(Calendar.HOUR_OF_DAY)
+            val minute = c.get(Calendar.MINUTE)
+
+            val tpd = TimePickerDialog(this, TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+
+                addTaskViewModel.setTime(hourOfDay.toString() + ":" + minute)
+
+            }, hour, minute, false)
+            tpd.show()
+        })
+
+        addTaskViewModel.addTaskEventLiveData.observe(this, Observer {
+            startActivity(Intent(this, HomeActivity::class.java))
+        })
     }
 }
+
