91ba1997b7de7096ad11660a8380737b0ccf810b
July Slavhev <jul.slavchev.tues19@gmail.com>
2019-01-17 16:40:57 +0200

    OpenedTaskActivity and layout created.

---
+++ app/src/main/java/com/example/user/jobche/OpenedTaskActivity.kt
@@ -0,0 +1,12 @@
+package com.example.user.jobche
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpenedTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_opened_task)
+    }
+}
