6cdefe14655292bda0c3809fd1a4ab75dbc19a7e
July Slavhev <jul.slavchev.tues19@gmail.com>
2019-02-07 00:34:08 +0200

    Task appliers activity viewmodel and layout created. Getting appliers for current task and put them into recycler view

---
+++ app/src/main/java/com/example/user/jobche/TaskAppliersViewModel.kt
@@ -0,0 +1,131 @@
+package com.example.user.jobche
+
+import android.arch.lifecycle.LiveData
+import android.telecom.Call
+import android.util.Log
+import com.example.user.jobche.Model.Application
+import com.example.user.jobche.Model.Applications
+import com.example.user.jobche.Model.UserProfile
+import okhttp3.Credentials
+import retrofit2.Callback
+import retrofit2.Response
+
+class TaskAppliersViewModel {
+
+    private var page: Int = 0
+
+    private val size: Int = 20
+
+    private lateinit var email: String
+
+    private lateinit var password: String
+
+    private var taskId: Int = 0
+
+    private var appliers = ArrayList<UserProfile>()
+
+    private var applications = ArrayList<Application>()
+
+    private val _adapterEventLiveData = SingleLiveData<Any>()
+
+    private val _updateAdapterEventLiveData = SingleLiveData<Any>()
+
+
+
+    fun getEmail(): String {
+        return this.email
+    }
+
+    fun setEmail(email: String) {
+        this.email = email
+    }
+
+    fun getPassword(): String {
+        return this.password
+    }
+
+    fun setPassword(password: String) {
+        this.password = password
+    }
+
+    fun getPage(): Int {
+        return this.page
+    }
+
+    fun setPage(page: Int) {
+        this.page = page
+    }
+
+    fun getSize(): Int {
+        return this.size
+    }
+
+    fun getAuthToken(): String {
+        return Credentials.basic(getEmail(), getPassword())
+    }
+
+    fun getTaskId(): Int {
+        Log.d("VTOROTV", taskId.toString())
+        return this.taskId
+    }
+
+    fun setTaskId(taskId: Int) {
+        this.taskId = taskId
+        Log.d("PYROV", taskId.toString())
+    }
+
+
+    fun getAppliers() : ArrayList<UserProfile> {
+        return this.appliers
+    }
+
+    fun getApplications(): ArrayList<Application> {
+        return this.applications
+    }
+
+    fun setApplications(applications: ArrayList<Application> ) {
+        this.applications = applications
+    }
+
+
+    val adapterEventData: LiveData<Any>
+        get() = _adapterEventLiveData
+
+    val updateAdapterEventLiveData: LiveData<Any>
+        get() = _updateAdapterEventLiveData
+
+
+    fun getTaskAppliers() {
+        val call = RetrofitClient().getApi()
+            .getAppliers(getAuthToken(), getTaskId(), getPage(), getSize())
+
+        call.enqueue(object : Callback<Applications> {
+            override fun onFailure(call: retrofit2.Call<Applications>, t: Throwable) {
+                Log.d("My Apply onFailure ", t.message.toString())
+            }
+
+            override fun onResponse(call: retrofit2.Call<Applications>, response: Response<Applications>) {
+                Log.d("My Apply onSuccess", response.body().toString())
+                if (response.body() != null) {
+                    setApplications(response.body()!!.applications)
+                    for (appl in getApplications()) {
+                        getAppliers().add(appl.applicant)
+                    }
+                    if (getPage() == 0) {
+                        _adapterEventLiveData.call()
+                    } else {
+                        _updateAdapterEventLiveData.call()
+                    }
+                } else if (getPage() == 0) {
+                    Log.d("RecylerView", "It's Empty!")
+
+                } else {
+                    Log.d("RecylerView", "No more tasks to show!")
+                    setPage(getPage() - 1)
+                }
+            }
+        })
+    }
+
+}
+
---
+++ app/src/main/java/com/example/user/jobche/UI/TaskAppliersActivity.kt
@@ -0,0 +1,74 @@
+package com.example.user.jobche.UI
+
+import android.arch.lifecycle.Observer
+import android.content.SharedPreferences
+import android.databinding.DataBindingUtil
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import com.example.user.jobche.AppliersRecyclerViewAdapter
+import com.example.user.jobche.TaskAppliersViewModel
+import com.example.user.jobche.R
+import com.example.user.jobche.databinding.ActivityTaskAppliersBinding
+
+class TaskAppliersActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var email: String
+    private lateinit var password: String
+    private var page = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_appliers)
+
+        val sharedPreferences: SharedPreferences = getSharedPreferences("SHARED_PREFS", MODE_PRIVATE)
+        email = sharedPreferences.getString("EMAIL", "")!!
+        password = sharedPreferences.getString("PASSWORD", "")!!
+
+        val taskId:Int = intent.getIntExtra("TaskId", 0)
+        Log.d("IDTASK", taskId.toString())
+
+        val binding: ActivityTaskAppliersBinding = DataBindingUtil.setContentView(this, R.layout.activity_task_appliers)
+        val taskAppliersViewModel = TaskAppliersViewModel()
+        binding.viewModel = taskAppliersViewModel
+
+        taskAppliersViewModel.setEmail(email)
+        taskAppliersViewModel.setPassword(password)
+        taskAppliersViewModel.setTaskId(taskId)
+
+        recyclerView = binding.listOfAppliers
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+
+        taskAppliersViewModel.getTaskAppliers()
+
+        taskAppliersViewModel.adapterEventData.observe(this, Observer {
+            recyclerView.adapter = AppliersRecyclerViewAdapter(
+                this,
+                taskAppliersViewModel.getAppliers()
+            )
+        })
+
+
+        taskAppliersViewModel.updateAdapterEventLiveData.observe(this, Observer {
+            recyclerView.adapter!!.notifyDataSetChanged()
+
+        })
+
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (!recyclerView.canScrollVertically(1)) {
+                    page += 1
+                    taskAppliersViewModel.setPage(page)
+                    taskAppliersViewModel.getAppliers()
+                }
+            }
+        })
+    }
+
+}
