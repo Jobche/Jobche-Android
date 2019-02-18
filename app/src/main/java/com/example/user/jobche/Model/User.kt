d09da2e7dee74e425956ebc6eca5252d278977c2
July Slavhev <jul.slavchev.tues19@gmail.com>
2019-02-19 00:58:45 +0200

    User Model is created and used to get parameters from backend.

--- app/src/main/java/com/example/user/jobche/Model/User.kt
+++ app/src/main/java/com/example/user/jobche/Model/User.kt
@@ -2,13 +2,15 @@ package com.example.user.jobche.Model
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
-import org.joda.time.DateTime
 import org.joda.time.LocalDate
 
 @Parcelize
-data class User(var id:Int = 0,
-                var firstName: String? = "",
-                var lastName: String? = "",
-                var email: String? = "",
-                var password: String? = "",
-                var dateOfBirth: LocalDate? = null) : Parcelable
+data class User(
+     val id: Int = 0,
+     var firstName: String = "",
+     var lastName: String = "",
+     var password: String = "",
+     var dateOfBirth: DateOfBirth?  = null,
+     var email: String = "",
+     var phoneNum: String = ""
+) : Parcelable
--- app/src/main/java/com/example/user/jobche/Model/UserProfile.kt
+++ app/src/main/java/com/example/user/jobche/Model/UserProfile.kt
@@ -1,6 +1,9 @@
 package com.example.user.jobche.Model
 
+import org.joda.time.LocalDate
+
 data class UserProfile(val id:Int,
                        val firstName: String,
                        val lastName: String,
-                       val dateOfBirth: DateOfBirth)
+                       val dateOfBirth: LocalDate
+)
